02e3f30edde74d68ef94b41f2a86fc5e1a488711
Szőke Roland <szokeroland0@gmail.com>
2024-03-16 11:11:11 +0100

    Initial commit

---
+++ src/main/java/hu/webler/weblerjwtstorage/WeblerJwtStorageApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerjwtstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerJwtStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblerJwtStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/webler/weblerjwtstorage/WeblerJwtStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerjwtstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerJwtStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
